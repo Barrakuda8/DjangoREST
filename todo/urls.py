6264771605e88dac3021a84d019b0334fdad2f6f
Barrakuda8 <anime.vegan@gmail.com>
2022-12-07 14:29:57 +0300

    First commit

--- todo/urls.py
+++ todo/urls.py
@@ -1,10 +1,25 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework.permissions import AllowAny
 from userapp.views import UserModelViewSet
 from todoapp.views import ProjectModelViewSet, TodoModelViewSet
 from rest_framework.authtoken import views
 
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Todo",
+        default_version='0.9',
+        description="Documentation for Todo project",
+        contact=openapi.Contact(email="admin@admin.local"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=[AllowAny],
+)
 
 router = DefaultRouter()
 router.register('users', UserModelViewSet)
@@ -15,5 +30,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include(router.urls)),
-    path('api-token-auth/', views.obtain_auth_token)
+    path('api-token-auth/', views.obtain_auth_token),
+    path('api/<str:version>/users/', UserModelViewSet.as_view({'get': 'list'})),
+    path('swagger<str:format>/', schema_view.without_ui()),
+    path('swagger/', schema_view.with_ui('swagger')),
 ]
--- userapp/views.py
+++ userapp/views.py
@@ -1,12 +1,16 @@
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import mixins
-from .serializers import UserModelSerializer
+from .serializers import UserModelSerializer, UserModelSerializerWithStaff
 from .models import User
 
 
 class UserModelViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, GenericViewSet):
 
     queryset = User.objects.all()
-    serializer_class = UserModelSerializer
     permission_classes = [IsAuthenticated]
+
+    def get_serializer_class(self):
+        if self.request.version == 'v2':
+            return UserModelSerializerWithStaff
+        return UserModelSerializer
